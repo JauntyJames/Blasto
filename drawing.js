98339d18ef3a9f199529581f22467f3af3bc1bbf
James Hartman <hartman.james.p@gmail.com>
2018-03-13 09:07:46 -0400

    losing track of canvas state

--- drawing.js
+++ drawing.js
@@ -7,7 +7,7 @@ function draw_grid(ctx, minor, major, stroke, fill) {
   ctx.strokeStyle = stroke;
   ctx.fillStyle = fill;
   let width = ctx.canvas.width, height = ctx.canvas.height
-  for(var x = 0; x < canvas.width; x += minor) {
+  for(var x = 0; x < width; x += minor) {
     ctx.beginPath();
     ctx.moveTo(x, 0);
     ctx.lineTo(x, height);
@@ -15,13 +15,57 @@ function draw_grid(ctx, minor, major, stroke, fill) {
     ctx.stroke();
     if(x % major === 0) { ctx.fillText(x, x, 10) }
   }
-  for(var y = 0; y < canvas.height; y += 10) {
+  for(var y = 0; y < height; y += 10) {
     ctx.beginPath();
     ctx.moveTo(0, y);
-    ctx.lineTo(canvas.width, y);
+    ctx.lineTo(width, y);
     ctx.lineWidth = (y % 50 == 0) ? 0.5 : 0.25;
     ctx.stroke();
     if(y % major === 0) { ctx.fillText(y, 0, y + 10) }
   }
   ctx.restore(); // restores canvas variables to their state at start of func
+}
+
+function draw_pacman(context, x, y, radius, o) {
+  context.save();
+  context.beginPath();
+  context.arc(x, y, radius, (o * Math.PI * 0.2), -(o * Math.PI * 0.2));
+  context.lineTo(x, y);
+  context.fillStyle = "yellow";
+  context.fill();
+  context.closePath();
+  context.stroke();
+  context.restore();
 } 
+
+function draw_ship(ctx, radius, options) {
+  options = options || {};
+  ctx.save();
+  if (options.guide) {
+    ctx.strokeStyle = "white";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+    ctx.lineWidth = 0.5;
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI)
+    ctx.stroke();
+    ctx.fill();
+  }
+  ctx.lineWidth = options.lineWidth || 2;
+  ctx.strokeStyle = options.stroke || "white";
+  ctx.fillStyle = options.fill || "black";
+  let angle = (options.angle || 0.5 * Math.PI) / 2;
+  ctx.beginPath();
+  ctx.moveTo(radius, 0);
+  ctx.lineTo(
+    Math.cos(Math.PI - angle) * radius,
+    Math.sin(Math.PI - angle) * radius
+  );
+  ctx.lineTo(
+    Math.cos(Math.PI + angle) * radius,
+    Math.sin(Math.PI + angle) * radius
+  );
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  ctx.restore();
+}
